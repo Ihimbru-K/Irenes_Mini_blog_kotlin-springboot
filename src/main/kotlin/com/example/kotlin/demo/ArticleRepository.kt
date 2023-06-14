3c36f78ceac8bbebe1a075ca730c059b19ecebad
Ihimbru-k <kanihims25@gmail.com>
2023-06-14 12:03:18 +0100

    implemented model and controller methods

---
+++ src/main/kotlin/com/example/kotlin/demo/ArticleRepository.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin.demo
+
+import com.example.kotlin.demo.model.Article
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+/*This interface inherits all CRUD methods from JpaRepository
+*Used to access data from the datasource
+ */
+@Repository
+interface ArticleRepository : JpaRepository<Article, Long>
---
+++ src/main/kotlin/com/example/kotlin/demo/Controller/ArticleController.kt
@@ -0,0 +1,53 @@
+package com.example.kotlin.demo.Controller
+
+import com.example.kotlin.demo.ArticleRepository
+import com.example.kotlin.demo.model.Article
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.http.HttpStatus
+
+@RestController
+@RequestMapping("/api")
+class ArticleController(private val articleRepository: ArticleRepository) {
+
+
+    //return a list of articles
+    @GetMapping("/articles")
+    fun getArticles(): MutableList<Article> = articleRepository.findAll()
+
+    @PostMapping("/articles")
+    fun createNewArticle(article: Article) : Article = articleRepository.save(article)
+
+    @GetMapping("/articles/{id}")
+    fun getArticleById(@PathVariable(value = "id") articleId: Long): ResponseEntity<Article> {
+        return articleRepository.findById(articleId).map {
+          article ->  ResponseEntity.ok(article)
+        }.orElse(ResponseEntity.notFound().build())
+    }
+
+    @PutMapping("/articles/{id}")
+    fun updateArticleById(@PathVariable(value = "id") articleId: Long, @Valid @RequestBody newArticle: Article) : ResponseEntity<Article>{
+        return articleRepository.findById(articleId).map { existingArticle ->
+            val updatedArticle: Article = existingArticle
+                .copy(title = newArticle.title, content = newArticle.content)
+            ResponseEntity.ok().body(articleRepository.save(updatedArticle))
+        }.orElse(ResponseEntity.notFound().build())
+
+    }
+
+    @DeleteMapping("/articles{id}")
+    fun deleteArticleById(@PathVariable(value = "id") articleId: Long) : ResponseEntity<Void>{
+        return articleRepository.findById(articleId).map { article  ->
+            articleRepository.delete(article)
+            ResponseEntity<Void>(HttpStatus.OK)
+        }.orElse(ResponseEntity.notFound().build())
+
+
+    }
+
+
+
+
+
+}
