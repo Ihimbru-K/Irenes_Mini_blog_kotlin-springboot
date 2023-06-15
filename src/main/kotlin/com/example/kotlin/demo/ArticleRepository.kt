1f735c02e2927878d7518ecdae54718a0d4bffe0
Ihimbru-k <kanihims25@gmail.com>
2023-06-15 10:58:41 +0100

    added some documentation

--- src/main/kotlin/com/example/kotlin/demo/ArticleRepository.kt
+++ src/main/kotlin/com/example/kotlin/demo/ArticleRepository.kt
@@ -4,7 +4,12 @@ import com.example.kotlin.demo.model.Article
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
-/*This interface inherits all CRUD methods from JpaRepository
+/*
+This block of code defines an interface called ArticleRepository.
+The @Repository annotation indicates that this interface is a repository that can be used to access data from the datasource.
+The JpaRepository<Article, Long> specifies that this repository is a JPA repository for
+the Article entity and its primary key is of type Long.
+This interface inherits all CRUD methods from JpaRepository
 *Used to access data from the datasource
  */
 @Repository
--- src/main/kotlin/com/example/kotlin/demo/Controller/ArticleController.kt
+++ src/main/kotlin/com/example/kotlin/demo/Controller/ArticleController.kt
@@ -12,13 +12,30 @@ import org.springframework.http.HttpStatus
 class ArticleController(private val articleRepository: ArticleRepository) {
 
 
-    //return a list of articles
+    //return a mutable list of articles
+    /**
+     * The @GetMapping("/articles") annotation specifies that this function will handle GET requests with the /articles suffix.
+     * The function returns a mutable list of articles by calling
+     * the findAll() method on the articleRepository.
+     * */
     @GetMapping("/articles")
     fun getArticles(): MutableList<Article> = articleRepository.findAll()
 
+
+    /**
+     * This block of code defines a function called createNewArticle().
+     * The @PostMapping("/articles") annotation specifies that this function will handle POST requests with the /articles suffix.
+     * The function takes an instance of an article as input and saves it to the database by calling the save() method on the articleRepository.
+     * */
     @PostMapping("/articles")
     fun createNewArticle(article: Article) : Article = articleRepository.save(article)
 
+
+    /**
+     * This block of code defines a function called getArticleById().
+     * The @GetMapping("/articles/{id}") annotation specifies that this function will handle GET requests with the /articles/{id} suffix.
+     * The function takes an ID as input and returns an instance of an article by calling the findById() method on the articleRepository.
+     * */
     @GetMapping("/articles/{id}")
     fun getArticleById(@PathVariable(value = "id") articleId: Long): ResponseEntity<Article> {
         return articleRepository.findById(articleId).map {
@@ -26,6 +43,12 @@ class ArticleController(private val articleRepository: ArticleRepository) {
         }.orElse(ResponseEntity.notFound().build())
     }
 
+
+    /**
+     * This block of code defines a function called updateArticleById().
+     * The @PutMapping("/articles/{id}") annotation specifies that this function will handle PUT requests with the /articles/{id} suffix.
+     * The function takes an ID and an instance of an article as input and updates it in the database by calling the save() method on the articleRepository.
+     * */
     @PutMapping("/articles/{id}")
     fun updateArticleById(@PathVariable(value = "id") articleId: Long, @Valid @RequestBody newArticle: Article) : ResponseEntity<Article>{
         return articleRepository.findById(articleId).map { existingArticle ->
@@ -36,6 +59,12 @@ class ArticleController(private val articleRepository: ArticleRepository) {
 
     }
 
+
+    /**
+     * This block of code defines a function called deleteArticleById().
+     * The @DeleteMapping("/articles{id}") annotation specifies that this function will handle DELETE requests with the /articles/{id} suffix.
+     * The function takes an ID as input and deletes it from the database by calling the delete() method on the articleRepository.
+     * */
     @DeleteMapping("/articles{id}")
     fun deleteArticleById(@PathVariable(value = "id") articleId: Long) : ResponseEntity<Void>{
         return articleRepository.findById(articleId).map { article  ->
@@ -48,16 +77,6 @@ class ArticleController(private val articleRepository: ArticleRepository) {
 
 
 
-
-    if (Serial.available()) {
-        char incomingData = Serial.read();
-        if (incomingData == '0' ) {
-            angle = 360;
-        } else {
-            angle = '1';
-        }
-        myservo.write(angle);
-    }
 
 
 
