40825c46d9364df0902615069391278c97c8809f
Ihimbru Kanyimi <87714194+Ihimbru-K@users.noreply.github.com>
2024-12-11 15:18:48 +0100

    Update ArticleRepository.kt

--- src/main/kotlin/com/example/kotlin/demo/ArticleRepository.kt
+++ src/main/kotlin/com/example/kotlin/demo/ArticleRepository.kt
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository
 
 /*This interface inherits all CRUD methods from JpaRepository
 *Used to access data from the datasource
- */..............
+ */.............
 @Repository
 interface ArticleRepository : JpaRepository<Article, Long>
